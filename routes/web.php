053c79737431fd251859c84ac68de7e5f69ddadb
Hasan Ahmed <hasan.ahmed1810@gmail.com>
2022-02-08 19:34:17 +0500

    commit 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/upload', function(){
-    return view('upload');
-});
+Route::get('/upload', [UploadController::class, 'index']);
+
+Route::post('/upload', [UploadController::class, 'upload']);
 
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
